bfd4dee9ccd5879be2eb9e136cd49e8ac66392c1
YaPopovAndrey <popov-1710@ya.ru>
2021-09-22 21:08:10 +0300

    add controller login, edit userShema

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,25 @@
 /* eslint-disable linebreak-style */
+const jwt = require('jsonwebtoken');
 const validator = require('validator');
 const User = require('../models/user');
+
+module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+
+      res
+        .status(200)
+        .send({ token });
+    })
+    .catch((err) => {
+      res
+        .status(401)
+        .send({ message: err.message });
+    });
+};
 
 module.exports.getUsers = (req, res) => {
   User.find({})
--- models/user.js
+++ models/user.js
@@ -1,5 +1,8 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable func-names */
+/* eslint-disable import/no-unresolved */
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userShema = new mongoose.Schema({
   email: {
@@ -29,5 +32,23 @@ const userShema = new mongoose.Schema({
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 }, { versionKey: false });
+
+userShema.statics.findUserByCredentials = function (email, password) {
+  return this.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+
+          return user;
+        });
+    });
+};
 
 module.exports = mongoose.model('user', userShema);
